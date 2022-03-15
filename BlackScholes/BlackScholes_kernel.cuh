aa2739d9a218dd0322f54195368f4c394fc83de7
Aditya <akamath1997@gmail.com>
2022-03-15 12:00:35 -0700

    Modify BS to not use GPU intrinsics

--- BlackScholes/BlackScholes_kernel.cuh
+++ BlackScholes/BlackScholes_kernel.cuh
@@ -24,10 +24,10 @@ __device__ inline float cndGPU(float d)
     const float RSQRT2PI = 0.39894228040143267793994605993438f;
 
     float
-    K = __fdividef(1.0f, (1.0f + 0.2316419f * fabsf(d)));
+    K = 1.0f / (1.0f + 0.2316419f * fabsf(d));
 
     float
-    cnd = RSQRT2PI * __expf(- 0.5f * d * d) *
+    cnd = RSQRT2PI * expf(- 0.5f * d * d) *
           (K * (A1 + K * (A2 + K * (A3 + K * (A4 + K * A5)))));
 
     if (d > 0)
@@ -53,15 +53,15 @@ __device__ inline void BlackScholesBodyGPU(
     float sqrtT, expRT;
     float d1, d2, CNDD1, CNDD2;
 
-    sqrtT = __fdividef(1.0F, rsqrtf(T));
-    d1 = __fdividef(__logf(S / X) + (R + 0.5f * V * V) * T, V * sqrtT);
+    sqrtT = 1.0F / rsqrtf(T);
+    d1 = (logf(S / X) + (R + 0.5f * V * V) * T) / (V * sqrtT);
     d2 = d1 - V * sqrtT;
 
     CNDD1 = cndGPU(d1);
     CNDD2 = cndGPU(d2);
 
     //Calculate Call and Put simultaneously
-    expRT = __expf(- R * T);
+    expRT = expf(- R * T);
     CallResult = S * CNDD1 - X * expRT * CNDD2;
     PutResult  = X * expRT * (1.0f - CNDD2) - S * (1.0f - CNDD1);
 }
